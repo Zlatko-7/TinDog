6245f5bb0b2eeb6b10bbf747f848e67023636e53
Zlatko-7 <108333845+Zlatko-7@users.noreply.github.com>
2023-05-02 21:47:47 +0200

    Add files via upload

---
+++ Dog.js
@@ -0,0 +1,27 @@
+import dogs from './data.js'
+
+
+class Dog{
+    constructor(data) {
+        Object.assign(this, data)
+    }
+    isMatch(bool) {
+        this.hasBeenSwiped = bool
+        this.hasBeenLiked = false
+    }
+    
+    dogHtml() {
+        const { name, avatar, age, bio } = this 
+        return `<div>
+                <div class='dog-info'>
+                <h4>${name} , ${age}</h4>
+                <p class='dog-bio'>${bio}</p>
+                 </div>
+                 <div>
+                <img class='dog-img' src='${avatar}'> 
+                </div>
+            </div>`
+    }
+}
+
+export default Dog
---
+++ index.js
@@ -0,0 +1,52 @@
+import dogs from './data.js'
+import Dog from './Dog.js'
+
+let currentIndex = 0
+let currentDog = new Dog(dogs[currentIndex])
+
+const liked = document.getElementById('liked')
+const reject = document.getElementById('reject')
+
+document.getElementById('like-btn').addEventListener('click', likeDog)
+document.getElementById('reject-btn').addEventListener('click', rejectDog)
+
+function getNextDog() {
+      currentIndex += 1
+      currentDog = new Dog(dogs[currentIndex])
+      render()
+
+    if(currentIndex === dogs.length) {
+      document.body.innerHTML = `<div class='end-message'>There are no more dogs in the area</div>`
+    }
+    
+    liked.innerHTML = ''
+    reject.innerHTML = ''
+ }
+
+function likeDog() {
+      liked.innerHTML = `<img class='liked' src="images/badge-like.png">`
+      setTimeout(() => getNextDog(), 2000)
+    }
+  
+
+function rejectDog() {
+    reject.innerHTML = `<img class='reject' src="images/badge-nope.png">`
+    setTimeout(() => getNextDog(), 2000)
+  } 
+
+function noDisplay() {
+    document.getElementById('liked').style.display = 'none'    
+}
+   
+function render() {
+document.getElementById('card').innerHTML = currentDog.dogHtml()
+}
+
+render()
+
+
+
+
+
+
+
